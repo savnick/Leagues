234e83b3818d51908473a362c75165bc04bbc399
Nick Savenkov <savnick@icloud.com>
2022-07-09 11:41:29 +0300

    Initial Commit

---
+++ Leagues/LeaguesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaguesApp.swift
+//  Leagues
+//
+//  Created by Nick on 09.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct LeaguesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
